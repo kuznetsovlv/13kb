3e056cfdb53dd83662199a9dfabb62a8e39c933c
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-07-10 22:15:14 +0300

    index.js: Zone class shared

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 
 export {default as Canvas}		from './canvas/Canvas';
 export {default as Item}		from './canvas/Item';
+export {default as Zone}		from './canvas/Zone';
 export {default as animator}	from './animate/animator';
