8029e882950245b91f7bbf637ae1d11b1eee61a6
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-07-24 02:53:08 +0300

    KeyMap put in api

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export {default as Node}	    from './node/Node';
 export {default as animator}	from './animate/animator';
 export {default as expander}	from './lib/expander';
 export {default as events}	    from './events/events';
+export {default as keymap}	    from './events/keymap';
