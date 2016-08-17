12b7345bd2a57a6835e38ac9c87da83cf36b2183
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-08-18 00:26:51 +0300

    Cosmetic fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-export {default as Document}	   from './document/Document';
-export {default as Node}	       from './node/Node';
-export {default as animator}	   from './animate/animator';
-export {default as expander}	   from './lib/expander';
-export {default as eventInterfase} from './interfaces/events';
-export {default as keymap}	       from './lib/keymap';
-export {default as eventFactory}   from './event/event-factory';
+export {default as Document}		from './document/Document';
+export {default as Node}			from './node/Node';
+export {default as animator}		from './animate/animator';
+export {default as expander}		from './lib/expander';
+export {default as eventInterfase}	from './interfaces/events';
+export {default as keymap}			from './lib/keymap';
+export {default as eventFactory}	from './event/event-factory';
